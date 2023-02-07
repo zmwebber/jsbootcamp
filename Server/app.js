a9aaadc006f3cb49817b5f1bcbd0df2a5bb588b2
zwebber <zwebber@eliassen.com>
2023-02-07 13:14:11 -0500

    Initial Commit
    CRA - typescript redux
    Node express with nodemon

---
+++ Server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const port = 8000;
+
+dotenv.config();
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+})
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
